6e4d85b86d1f847bffd02e56787c647302fa415f
LeviHernandez315 <levi.hernandez@unah.hn>
2024-12-14 03:56:02 -0600

    prueba unitaria de multiplicar agregada

--- tests/unit/app.unit.test.js
+++ tests/unit/app.unit.test.js
@@ -4,3 +4,7 @@ const utils = require('../../src/index');
 test('Debe sumar correctamente', () => {
     expect(utils.suma(2, 3)).toBe(5);
 });
+
+test('Debe multiplicar dos números correctamente', () => {
+    expect(utils.multiplica(4, 5)).toBe(20);
+});
